fdaab62931fedf865e113477ad1fba3ae95e85ed
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-05 02:13:10 +0300

    feat: implement get city location by name in repository and data source

--- src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
@@ -1,13 +1,14 @@
 package data.repository
 
+import data.repository.mapper.toCityLocation
 import logic.model.CityLocation
 import logic.repository.WeatherRepository
 
 class WeatherRepositoryImpl(
-    val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource
 ) : WeatherRepository {
 
     override suspend fun getCityLocationByName(cityName: String): CityLocation {
-        return CityLocation(name = "Egypt", latitude = 0.0, longitude = 0.0)
+        return remoteDataSource.getCityLocationByName(cityName).toCityLocation()
     }
 }
