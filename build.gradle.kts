f76714c9abb74c9ff89c3174dcf8b5ae4478aa3a
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-04 23:37:16 +0300

    feat: add coroutines and ktor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,15 @@ repositories {
 
 dependencies {
     implementation("io.insert-koin:koin-core:4.0.3")
+    implementation("io.ktor:ktor-client-core:2.3.13")
+    implementation("io.ktor:ktor-client-cio:2.3.13")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation(kotlin("test"))
     testImplementation("com.google.truth:truth:1.4.4")
     testImplementation("io.mockk:mockk:1.14.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
 
 kover {
