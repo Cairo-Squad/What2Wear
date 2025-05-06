7f91b5f99c26c65e2d9a11e9e66d234f4c781a5d
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-06 15:11:35 +0300

    fix: add default values for all response attributes to avoid MissingFieldException, and extract the base url to a constant

---
+++ src/main/kotlin/data/datasource/ApiConstants.kt
@@ -0,0 +1,5 @@
+package data.datasource
+
+object ApiConstants {
+    const val GEOCODING_BASE_URL = "https://geocoding-api.open-meteo.com/v1"
+}
--- src/main/kotlin/data/repository/mapper/CityLocationMapper.kt
+++ src/main/kotlin/data/repository/mapper/CityLocationMapper.kt
@@ -6,8 +6,8 @@ import logic.model.CityLocation
 fun CityLocationResponse.toCityLocation(): CityLocation {
     val firstCityLocation = this.results.first()
     return CityLocation(
-        name = firstCityLocation.name,
-        latitude = firstCityLocation.latitude,
-        longitude = firstCityLocation.longitude
+        name = firstCityLocation.name!!,
+        latitude = firstCityLocation.latitude!!,
+        longitude = firstCityLocation.longitude!!
     )
 }
