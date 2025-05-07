1a44dd313ddfff325b03b468fef4d762da33503e
mohamed_Galal <az.galal666@gmail.com>
2025-05-07 09:49:42 +0300

    feat: add Ktor client implementation in data source

--- src/main/kotlin/data/datasource/RemoteDataSourceImpl.kt
+++ src/main/kotlin/data/datasource/RemoteDataSourceImpl.kt
@@ -1,9 +1,27 @@
 package data.datasource
 
+import data.datasource.ApiConstants.WEATHER_BASE_URL
+import data.dto.CurrentWeatherResponse
 import data.repository.RemoteDataSource
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
+import logic.model.LatLong
 
 class RemoteDataSourceImpl : RemoteDataSource {
     private val ktorClient = HttpClient(CIO)
+    override suspend fun getWeatherByLocation(latLong: LatLong): CurrentWeatherResponse {
+        val response =
+            ktorClient.get {
+                url(WEATHER_BASE_URL)
+                parameter("latitude", latLong.latitude)
+                parameter("longitude", latLong.longitude)
+                parameter("current_weather", "true")
+            }
+        return Json.decodeFromString<CurrentWeatherResponse>(response.bodyAsText())
+    }
 }
--- src/main/kotlin/data/repository/RemoteDataSource.kt
+++ src/main/kotlin/data/repository/RemoteDataSource.kt
@@ -1,4 +1,8 @@
 package data.repository
 
+import data.dto.CurrentWeatherResponse
+import logic.model.LatLong
+
 interface RemoteDataSource {
+    suspend fun getWeatherByLocation(latLong: LatLong): CurrentWeatherResponse
 }
