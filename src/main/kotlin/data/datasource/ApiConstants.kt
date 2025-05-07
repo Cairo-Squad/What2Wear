25afc87387aa1363081a0e74b901ab87f03b98a7
mohamed_Galal <az.galal666@gmail.com>
2025-05-07 09:53:22 +0300

    feat: add ApiConstants and koin for DI

---
+++ src/main/kotlin/data/datasource/ApiConstants.kt
@@ -0,0 +1,5 @@
+package data.datasource
+
+object ApiConstants {
+    const val WEATHER_BASE_URL = "https://api.open-meteo.com/v1/forecast"
+}
--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,7 +1,10 @@
 package di
 
+import data.datasource.RemoteDataSourceImpl
+import data.repository.WeatherRepositoryImpl
 import org.koin.dsl.module
 
 val appModule = module {
-
+    single { RemoteDataSourceImpl() }
+    single { WeatherRepositoryImpl(get()) }
 }
