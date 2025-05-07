8516f5d3d303b6ed1da07c4473187980ec510da4
mohamed_Galal <az.galal666@gmail.com>
2025-05-07 14:09:49 +0300

    feat: inject HttpClient in RemoteDataSourceImpl

--- src/main/kotlin/data/datasource/RemoteDataSourceImpl.kt
+++ src/main/kotlin/data/datasource/RemoteDataSourceImpl.kt
@@ -1,10 +1,10 @@
 package data.datasource
 
 import data.datasource.ApiConstants.WEATHER_BASE_URL
+import data.dto.CityLocationResponse
 import data.dto.CurrentWeatherResponse
 import data.repository.RemoteDataSource
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.url
@@ -12,8 +12,10 @@ import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.json.Json
 import logic.model.LatLong
 
-class RemoteDataSourceImpl : RemoteDataSource {
-    private val ktorClient = HttpClient(CIO)
+class RemoteDataSourceImpl(
+    private val ktorClient: HttpClient
+) : RemoteDataSource {
+
     override suspend fun getWeatherByLocation(latLong: LatLong): CurrentWeatherResponse {
         val response =
             ktorClient.get {
@@ -23,5 +25,9 @@ class RemoteDataSourceImpl : RemoteDataSource {
                 parameter("current_weather", "true")
             }
         return Json.decodeFromString<CurrentWeatherResponse>(response.bodyAsText())
+    }
+
+    override suspend fun getCityLocationByName(cityName: String): CityLocationResponse {
+        TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -2,11 +2,12 @@ package di
 
 import data.datasource.RemoteDataSourceImpl
 import data.repository.WeatherRepositoryImpl
-import data.repository.RemoteDataSource
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
 import org.koin.dsl.module
 
 val appModule = module {
-    single { RemoteDataSourceImpl() }
+    single { HttpClient(CIO) }
+    single { RemoteDataSourceImpl(get()) }
     single { WeatherRepositoryImpl(get()) }
-    single<RemoteDataSource> { RemoteDataSourceImpl() }
 }
