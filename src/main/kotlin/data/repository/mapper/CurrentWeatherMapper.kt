baced556d37315cbbbe8ba504428ce60fe7ca949
mohamed_Galal <az.galal666@gmail.com>
2025-05-07 09:52:30 +0300

    feat: add CurrentWeatherMapper

--- src/main/kotlin/data/repository/mapper/CurrentWeatherMapper.kt
+++ src/main/kotlin/data/repository/mapper/CurrentWeatherMapper.kt
@@ -1,18 +1,18 @@
 package data.repository.mapper
 
-import data.dto.CurrentWeatherDto
+import data.dto.CurrentWeatherDTO
 import data.utils.TemperatureNotFoundException
 import data.utils.WeatherStateException
 import kotlinx.datetime.LocalDateTime
 import logic.model.CurrentWeather
 
-fun CurrentWeatherDto.toCurrentWeather(): CurrentWeather {
+fun CurrentWeatherDTO.toCurrentWeather(): CurrentWeather {
     val parsedTime = LocalDateTime.parse(
         time ?: throw IllegalArgumentException("Missing timestamp"),
     ).time
     return CurrentWeather(
         temperature = this.temperature ?: throw TemperatureNotFoundException(),
         time = parsedTime,
-        weatherCode = this.weatherCode ?: throw WeatherStateException()
+        weatherCode = this.weathercode ?: throw WeatherStateException()
     )
 }
