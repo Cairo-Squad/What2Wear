1b72cc8e5c4a49140933afd30dd21bb3f8bd6a4a
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-07 18:08:49 +0300

    fix: change wrong local time import, add a setup function

--- src/test/kotlin/logic/usecase/ClothingSuggestionUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/ClothingSuggestionUseCaseTest.kt
@@ -2,18 +2,24 @@ package logic.usecase
 
 import org.junit.jupiter.api.Test
 import com.google.common.truth.Truth.assertThat
+import kotlinx.datetime.LocalTime
 import logic.model.CurrentWeather
 import logic.model.SuggestionClothes
-import java.time.LocalTime
+import org.junit.jupiter.api.BeforeEach
 
 class ClothingSuggestionUseCaseTest {
 
-    private val clothingSuggestion = ClothingSuggestionUseCase()
+    private lateinit var clothingSuggestion: ClothingSuggestionUseCase
+
+    @BeforeEach
+    fun setup() {
+        clothingSuggestion = ClothingSuggestionUseCase()
+    }
 
     @Test
-    fun `should suggestt clothes for cold morning when temperature is cold`() {
+    fun `should suggest clothes for cold morning when temperature is cold`() {
         // Given
-        val currentWeather = CurrentWeather(temperature = 2.0, time = LocalTime.of(7, 0), weatherCode = 1)
+        val currentWeather = CurrentWeather(temperature = 2.0, time = LocalTime(7, 0), weatherCode = 1)
 
         // When
         val clothingSuggestion = clothingSuggestion.suggestClothes(currentWeather)
@@ -26,13 +32,13 @@ class ClothingSuggestionUseCaseTest {
                 timeOfDay = "morning",
                 suggestionClothes = listOf("Coat 🧥", "Hat 👒")
             )
-        )    }
+        )
+    }
 
     @Test
     fun `should suggest clothes for freezing night when temperature is Freezing `() {
         // Given
-        val currentWeather = CurrentWeather(temperature = -3.0, time = LocalTime.of(22, 0), weatherCode = 2)
-
+        val currentWeather = CurrentWeather(temperature = -3.0, time = LocalTime(22, 0), weatherCode = 2)
 
         // When
         val suggestionClothes = clothingSuggestion.suggestClothes(currentWeather)
@@ -45,13 +51,13 @@ class ClothingSuggestionUseCaseTest {
                 timeOfDay = "night",
                 suggestionClothes = listOf("Coat 🧥", "Scarf 🧣", "Gloves 🧤", "Hat 👒")
             )
-        )    }
+        )
+    }
 
     @Test
     fun `should suggest clothes for cool morning when temperature is  cool`() {
         // Given
-        val currentWeather = CurrentWeather(temperature = 10.0, time = LocalTime.of(8, 0), weatherCode = 3)
-
+        val currentWeather = CurrentWeather(temperature = 10.0, time = LocalTime(8, 0), weatherCode = 3)
 
         // When
         val clothingSuggestion = clothingSuggestion.suggestClothes(currentWeather)
@@ -64,13 +70,13 @@ class ClothingSuggestionUseCaseTest {
                 timeOfDay = "morning",
                 suggestionClothes = listOf("Jacket 🧥", "Hoodie 👚", "Boots 👢")
             )
-        )    }
+        )
+    }
 
     @Test
     fun `should suggest clothes for cold night when temperature is cool`() {
         // Given
-        val currentWeather = CurrentWeather(temperature = 12.0, time = LocalTime.of(20, 0), weatherCode = 4)
-
+        val currentWeather = CurrentWeather(temperature = 12.0, time = LocalTime(20, 0), weatherCode = 4)
 
         // When
         val clothingSuggestion = clothingSuggestion.suggestClothes(currentWeather)
@@ -83,13 +89,13 @@ class ClothingSuggestionUseCaseTest {
                 timeOfDay = "night",
                 suggestionClothes = listOf("Hoodie 👚", "Scarf 🧣")
             )
-        )    }
+        )
+    }
 
     @Test
     fun `should suggest clothes for nice morning when temperature is sunny`() {
         // Given
-        val currentWeather = CurrentWeather(temperature = 20.0, time = LocalTime.of(10, 0), weatherCode = 5)
-
+        val currentWeather = CurrentWeather(temperature = 20.0, time = LocalTime(10, 0), weatherCode = 5)
 
         // When
         val clothingSuggestion = clothingSuggestion.suggestClothes(currentWeather)
@@ -108,8 +114,7 @@ class ClothingSuggestionUseCaseTest {
     @Test
     fun `should suggest clothes for warm night when temperature is hot  `() {
         // Given
-        val currentWeather = CurrentWeather(temperature = 28.0, time = LocalTime.of(23, 0), weatherCode = 6)
-
+        val currentWeather = CurrentWeather(temperature = 28.0, time = LocalTime(23, 0), weatherCode = 6)
 
         // When
         val clothingSuggestion = clothingSuggestion.suggestClothes(currentWeather)
