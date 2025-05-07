5f987e45f91081fdd4de08f361bac5af48647324
mohamed_Galal <az.galal666@gmail.com>
2025-05-07 09:53:50 +0300

    feat: add some koin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("io.insert-koin:koin-core:4.0.3")
     implementation("io.ktor:ktor-client-core:2.3.13")
     implementation("io.ktor:ktor-client-cio:2.3.13")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
     implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
