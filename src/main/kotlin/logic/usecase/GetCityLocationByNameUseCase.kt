9a47d94b49329245e7682ffb44975f1282da72cb
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-09 20:15:28 +0300

    refactor: remove validation for the use case arguments

--- src/main/kotlin/logic/usecase/GetCityLocationByNameUseCase.kt
+++ src/main/kotlin/logic/usecase/GetCityLocationByNameUseCase.kt
@@ -7,10 +7,6 @@ class GetCityLocationByNameUseCase(
     private val weatherRepository: WeatherRepository
 ) {
     suspend fun getCityLocation(cityName: String): CityLocation {
-        if (cityName.isBlank()) {
-            throw IllegalArgumentException("Please enter a valid city name!")
-        }
-
         return weatherRepository.getCityLocationByName(cityName)
     }
 }
