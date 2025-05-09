4959416859ab2f49e8f9b2cc73cc8300e25dde5d
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-09 20:19:50 +0300

    refactor: change the mapping logic, make all logic models attributes have default values, and move wrap function to the repository

--- src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repository/WeatherRepositoryImpl.kt
@@ -5,21 +5,39 @@ import logic.model.CurrentWeather
 import logic.repository.WeatherRepository
 import data.repository.mapper.toCityLocation
 import data.repository.mapper.toCurrentWeather
+import io.ktor.utils.io.errors.*
+import kotlinx.serialization.SerializationException
+import logic.utils.FetchingWeatherException
 import logic.utils.NetworkException
-import logic.utils.wrapCallWithTry
+import logic.utils.UnexpectedErrorException
 
 class WeatherRepositoryImpl(
     private val remoteDataSource: RemoteDataSource
 ) : WeatherRepository {
 
     override suspend fun getCityLocationByName(cityName: String): CityLocation {
-        return wrapCallWithTry { remoteDataSource.getCityLocationByName(cityName).toCityLocation() }
+        return wrapCallWithTry {
+            remoteDataSource.getCityLocationByName(cityName).cityLocationDtoList?.firstOrNull()?.toCityLocation()
+                ?: throw FetchingWeatherException()
+        }
     }
 
     override suspend fun getWeatherFromRemote(cityLocation: CityLocation): CurrentWeather {
         return wrapCallWithTry {
             remoteDataSource.getWeatherByLocation(cityLocation).currentWeather?.toCurrentWeather()
-                ?: throw NetworkException()
+                ?: throw FetchingWeatherException()
+        }
+    }
+
+    private suspend fun <T> wrapCallWithTry(call: suspend () -> T): T {
+        try {
+            return call()
+        } catch (exception: Exception) {
+            throw when (exception) {
+                is IOException -> NetworkException()
+                is NullPointerException, is SerializationException, is FetchingWeatherException -> FetchingWeatherException()
+                else -> UnexpectedErrorException()
+            }
         }
     }
 }
--- src/main/kotlin/data/repository/mapper/CityLocationMapper.kt
+++ src/main/kotlin/data/repository/mapper/CityLocationMapper.kt
@@ -1,12 +1,11 @@
 package data.repository.mapper
 
-import data.dto.CityLocationDto
+import data.dto.SingleCityLocationDto
 import logic.model.CityLocation
 
-fun CityLocationDto.toCityLocation(): CityLocation {
-    val firstCityLocation = this.cityLocationDtoList?.firstOrNull()
+fun SingleCityLocationDto.toCityLocation(): CityLocation {
     return CityLocation(
-        latitude = firstCityLocation?.latitude,
-        longitude = firstCityLocation?.longitude
+        latitude = this.latitude,
+        longitude = this.longitude
     )
 }
