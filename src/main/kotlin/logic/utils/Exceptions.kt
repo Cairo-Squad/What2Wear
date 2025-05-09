53e2c77ad4abfcaaae5b2d7dbece384159639792
Abdo Sharaf <bodyasharaf8@gmail.com>
2025-05-09 20:16:39 +0300

    refactor: remove unused exception

--- src/main/kotlin/logic/utils/Exceptions.kt
+++ src/main/kotlin/logic/utils/Exceptions.kt
@@ -1,7 +1,6 @@
 package logic.utils
 
 open class WeatherException(message: String) : Exception(message)
-class WeatherStateException : WeatherException("There is no Weather State") // TODO: This one!!
 class NetworkException : WeatherException("Network unavailable! Please check your network.")
 class FetchingWeatherException : WeatherException("Can't fetch this city's weather! Please try again.")
 class UnexpectedErrorException : WeatherException("Unexpected error happened! Please try again.")
